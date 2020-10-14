200037d4de0e4cdf739a5dcde243c5389219e647
seazdero <kamael.322@yandex.ru>
2020-10-14 17:26:28 +0500

    homework from the second lesson

---
+++ HwL2/HwL2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HwL2
+{
+    class Program
+    {
+        // Задание 5
+        // a) Написать программу, которая запрашивает массу и рост человека, 
+        // вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, 
+        // набрать вес или всё в норме.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите Ваш вес в кг: ");
+            double m = double.Parse(Console.ReadLine());
+            Console.Write("Введите Ваш рост в см: ");
+            double h = double.Parse(Console.ReadLine());
+            h /= 100;
+            Console.Write(Environment.NewLine + "Индекс массы тела: ");
+            double bmi = m / (h * h);
+            Console.WriteLine("{0:0.##}", bmi);
+            Console.WriteLine(norm(bmi, h));
+            Console.ReadKey();
+
+        }
+        static string norm(double BMI, double h)
+        {
+            string result = "";
+            if (BMI >= 16.79 && BMI <= 21.11)
+            {
+                result = "Ваш индекс массы тела в норме.";
+            }
+            else if(BMI < 16.79)
+            {
+                result = "Ваш индекс массы тела меньше нормы./nВам необходимо набрать вес.";
+            }
+            else if (BMI > 21.11)
+                {
+                result = "Ваш индекс массы тела больше нормы./nВам необходимо сбросить вес.";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HwL2/HwL2Task7a/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HwL2Task7a
+{
+    class Program
+    {
+        // Задание 7
+        // a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
+
+        static int GetInt()
+        {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int a))
+                    Console.WriteLine("Требуется числовое значение./nПопробуйте еще раз.");
+                else return a;
+        }
+
+        static void Recursion(int start, int end)
+        {
+            if(start == end)
+            {
+                Console.WriteLine("\b\b");
+                Console.WriteLine("");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(start + ", ");
+                start++;
+                Recursion(start, end);
+            }
+        }
+
+
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число а: ");
+            int a = GetInt();
+            Console.WriteLine("Введите число b: ");
+            int b = GetInt();
+
+            Console.WriteLine("Числа входящие в промежуток: ");
+            Recursion(a, b);
+            Console.ReadKey();
+        }
+    }
+}
