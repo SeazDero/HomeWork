dd48612e719433383e50722f9c7316fd5b2bbd68
seazdero <kamael.322@yandex.ru>
2020-10-24 16:46:13 +0500

    tasks for the fifth lesson

---
+++ HwL5Task1/HwL5Task1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HwL5Task1
+{
+    class Program
+    {
+        // Задание 1
+        // Создать программу, которая будет проверять корректность ввода логина. 
+        // Корректным логином будет строка от 2 до 10 символов, 
+        // содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+        // а) без использования регулярных выражений;
+
+
+        /// <summary>
+        /// Метод, для проверки склонения слова "попытка".
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        static string attempts(int a)
+        {
+            string x = "";
+            if (a % 10 == 1 && a != 11) x += "  попытка";
+            else
+                if ((a >= 2 && a <= 4) || (a >= 22 && a <= 24) || (a >= 32 && a <= 34) || (a >= 41 && a <= 45)) x += " попытки";
+            else
+                if ((a == 11) || (a >= 5 && a <= 20) || (a >= 25 && a <= 30) || (a >= 35 && a < 41) || (a > 44 && a < 51)) x += " попыток";
+            return x;
+        }
+
+        /// <summary>
+        /// Метод, для проверки корректности логина(латинские символы).
+        /// </summary>
+        /// <param name="letter"></param>
+        /// <returns></returns>
+        static bool LatinLetter(char letter)
+        {
+            int num = letter;
+            if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Метод, проверки валидности логина(лимит символов).
+        /// </summary>
+        /// <param name="login"></param>
+        /// <returns></returns>
+        static bool CheckLog(string login)
+        {
+            int length = login.Length;
+            if (length >= 2 && length <= 10)
+            {
+                bool check = true;
+                char letter = login[0];
+                if (Char.IsDigit(letter))
+                    return false;
+                for (int i = 1; i < length; i++)
+                {
+                    letter = login[i];
+                    if (!(Char.IsDigit(letter) || LatinLetter(letter)))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                    return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для проверки валидности логина.");
+            int AmountOfTries = 3;
+            do
+            {
+                Console.WriteLine("Введите логин: ");
+                string login = Console.ReadLine();
+
+                if (CheckLog(login))
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    AmountOfTries--;
+                    Console.WriteLine("Ввод логина неверен." +
+                        "\nЛогин должен содержать только буквы латинского алфавита и цифры;" +
+                        "\nЦифра не может быть первой;" +
+                        "\nДлина логина должна быть от 2 до 10 символов." 
+                        + Environment.NewLine + "У вас осталось" + AmountOfTries + attempts);
+
+                        
+                }
+            } while (AmountOfTries > 0);
+            Console.WriteLine("Логин валиден");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HwL5Task1/HwL5Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HwL5Task2
+{
+    class Program
+    {
+        // Задание 2
+        // Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+        // а) Вывести только те слова сообщения,  которые содержат не более n букв.
+        // б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+        // в) Найти самое длинное слово сообщения.
+        // г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+        // д) Создать метод, который производит частотный анализ текста.
+        // В качестве параметра в него передается массив слов и текст, 
+        // в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст.
+        // Здесь требуется использовать класс Dictionary.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для демонстрации класса Message.");
+            Console.WriteLine("\nТекст: \n" + Message.text);
+            Console.WriteLine("\nВыведем слова текста, содержащие не больше 3 букв: ");
+            Message.GetWordsByLength(3);
+            Console.WriteLine();
+            Console.WriteLine("\nУдаляемиз текста, слова заканчивающиеся на 'б': ");
+            Message.DeleteWordByEndChar('б');
+            Console.WriteLine();
+            Console.WriteLine("\nСамое длинное слово в тексте: " + Message.FindMaxLengthWord());
+            Console.WriteLine("Сформированна строка StringBuilder из самых длинных слов: \n" + Message.GetLongWordsString());
+            Console.WriteLine("\nЧастотный анализ текста: ");
+            string[] array = { "она", "куст", "гнездо", "легенда" };
+            Message.FrequencyAnalysis(array, Message.text);
+            Console.ReadKey();
+        }
+    }
+}
